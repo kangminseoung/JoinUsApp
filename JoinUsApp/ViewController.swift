67c6cc9d7074f787edf1d0c7ffad0f0e62feec86
kangminseoung <alstjd2356@gmail.com>
2025-03-13 13:15:11 +0900

    [init] : 프로젝트 파일 생성

---
+++ JoinUsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JoinUsApp
+//
+//  Created by 강민성 on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
